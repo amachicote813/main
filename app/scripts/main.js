654fae79724956323e369667f89968117734edfc
amachicote813 <amachicote813@knights.ucf.edu>
2017-04-10 06:31:29 -0400

    foundation started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
